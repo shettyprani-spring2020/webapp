630f8900f5b03036817b897a7a862cbfa6562ea2
Pranit Shetty <pranitshetty@gmail.com>
2020-04-03 16:01:44 -0400

    added ssl to sequelize

--- models/index.js
+++ models/index.js
@@ -19,26 +19,28 @@ if (env == "test") {
 } else {
   env = "development";
   const config = require(__dirname + "/../../config/config.json")[env];
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ssl: true,
+    host: config.host,
+    dialect: config.dialect,
+    dialectOptions: {
+      ssl: "Amazon RDS",
+    },
+  });
 }
 
 fs.readdirSync(__dirname)
-  .filter(file => {
+  .filter((file) => {
     return (
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     );
   })
-  .forEach(file => {
+  .forEach((file) => {
     const model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -49,7 +51,7 @@ sequelize
   .then(() => {
     console.log("Connection has been established successfully.");
   })
-  .catch(error => {
+  .catch((error) => {
     console.error("Unable to connect to the database:", error);
   });
 
